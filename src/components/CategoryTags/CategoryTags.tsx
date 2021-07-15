fa7e3163400dd433d205b405a210342a58377f41
jpacsai <judit.pacsai@gmail.com>
2021-07-15 14:48:09 +0200

    refactor: tidy up code

--- src/components/CategoryTags/CategoryTags.tsx
+++ src/components/CategoryTags/CategoryTags.tsx
@@ -4,16 +4,8 @@ import './CategoryTags.scss';
 
 import { Meal, Tag as TagType } from '../../data/types';
 
-type Props = {
-  meal: Meal;
-};
-
-type TagProps = {
-  tag: TagType;
-};
-
 const colors = [
-  { name: 'raspberry', color: '#bc5173', text: 'white', usage: TagType.DESSZERT, display: true },
+  { name: 'raspberry', color: '#bc5173', text: 'white', usage: TagType.DESSZERT },
   { name: 'raspberry_light', color: '#E4779D', text: 'white', usage: TagType.TORTA },
   { name: 'purple', color: '#C87DB0', text: 'white', usage: TagType.SÜTEMÉNY },
   { name: 'turquoise_dark', color: '#A781D8', text: 'white', usage: TagType.REGGELI },
@@ -31,37 +23,40 @@ const colors = [
 ];
 
 const getTagColor = (tag: TagType) => {
-  const c = colors.find((color) => color.usage === tag);
-  const defaultColors = {
-    background: 'lightsteelblue',
-    text: 'black',
+  const color = colors.find(({ usage }) => usage === tag);
+  return {
+    background: color?.color || 'lightsteelblue',
+    text: color?.text || 'black',
   };
-  return c
-    ? {
-        background: c.color,
-        text: c.text,
-      }
-    : defaultColors;
+};
+
+// ---------- Tag ---------- //
+
+type TagProps = {
+  tag: TagType;
 };
 
 const Tag: React.FC<TagProps> = ({ tag }) => {
-  const tagColor = useMemo(() => getTagColor(tag), [tag]);
-
+  const { background, text } = useMemo(() => getTagColor(tag), [tag]);
   return (
-    <div className="category-tag" style={{ backgroundColor: tagColor.background, color: tagColor.text }}>
+    <div className="category-tag" style={{ backgroundColor: background, color: text }}>
       {tag}
     </div>
   );
 };
 
-const CategoryTags: React.FC<Props> = ({ meal }) => {
-  return (
-    <div className="category-tags">
-      {meal.details.tags.map((tag, index) => (
-        <Tag key={`tag-${index}`} tag={tag} />
-      ))}
-    </div>
-  );
+// ---------- CategoryTags ---------- //
+
+type CategoryTagsProps = {
+  meal: Meal;
 };
+
+const CategoryTags: React.FC<CategoryTagsProps> = ({ meal }) => (
+  <div className="category-tags">
+    {meal.details.tags.map((tag, index) => (
+      <Tag key={`tag-${index}`} tag={tag} />
+    ))}
+  </div>
+);
 
 export default CategoryTags;
