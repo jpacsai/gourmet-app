f80342953818c20971b0a6258ff8e1424efcb73c
jpacsai <judit.pacsai@gmail.com>
2021-07-24 14:57:16 +0200

    feat: improve Popup header

--- src/components/common/Popup/Popup.tsx
+++ src/components/common/Popup/Popup.tsx
@@ -10,14 +10,18 @@ import './Popup.scss';
 type PopupProps = {
   isOpen: boolean;
   onClose: () => void;
+  header?: React.ReactNode;
   className?: string;
 };
 
-const Popup: React.FC<PopupProps> = ({ isOpen, onClose, children, className }) => {
+const Popup: React.FC<PopupProps> = ({ isOpen, onClose, header, children, className }) => {
   return (
     <Overlay isOpen={isOpen} onClose={onClose} className={classnames('popup', className)}>
       <Card className="popup__card" elevation={Elevation.TWO}>
-        <CornerIcon className="popup__close-icon" icon="cross" onClick={onClose} />
+        <div className={classnames('popup__header', header && 'header-content')}>
+          {header && <div>{header}</div>}
+          <CornerIcon className="popup__close-icon" icon="cross" onClick={onClose} />
+        </div>
         <div className="popup__card_content">{children}</div>
       </Card>
     </Overlay>
