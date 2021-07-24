0eca38bd6de2cef40075afd49279925eac3496a5
jpacsai <judit.pacsai@gmail.com>
2021-07-24 07:15:01 +0200

    feat: improve Page

--- src/components/common/Page/Page.tsx
+++ src/components/common/Page/Page.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import './Page.scss';
 
 type PageProps = {
   title: string;
+  className?: string;
 };
 
-const Page: React.FC<PageProps> = ({ title, children }) => {
+const Page: React.FC<PageProps> = ({ title, children, className }) => {
   return (
-    <div className="page">
+    <div className={classnames('page', className)}>
       <h2 className="page__title">{title}</h2>
       {children}
     </div>
