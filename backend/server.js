112dcc5b87cf2bdd9c1fa6f03cf8a3165acb97f3
jpacsai <judit.pacsai@gmail.com>
2021-08-21 23:28:22 +0200

    fix: typo #3

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('api/v1/meals', meals);
+app.use('/api/v1/meals', meals);
 app.use('*', (req, res) => res.status(404).json({ error: 'Not found' }));
 
 export default app;
